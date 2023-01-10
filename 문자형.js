c51ed66b2de2f543784ef9b11a3373b787009609
eunjaelim <ineunjae361@gmail.com>
2023-01-11 03:44:36 +0900

    문자형

---
+++ 문자형.js
@@ -0,0 +1,27 @@
+// 문자형
+// String, Number, boolean, null, undefined
+
+const name = "scalper";
+const age = 30;
+const isMale = true;
+const isFemale = false;
+let money = null;
+let girlfriend = undefined;
+
+//console.log("my name is " + name + "and i am " + age ) //파이썬 f'내 이름은 {name}이다.'
+//console.log(`내 이름은 ${name} 이고 나이는 ${age} 입니다.`)
+
+const animal = "tiger";
+
+//console.log(animal.substring(0,2)); // 문자열 자르기
+//console.log(animal.substring(2,5))
+
+let fruits = "사과, 딸기, 포도, 참외";
+
+fruits = fruits.split(", "); // 공백까지 포함하여 자르기
+console.log(fruits[2]);
+
+
+
+
+
