3d1176e907e70d1fb04c7ab5e0a497c62e870821
eunjaelim <imeunjae361@gmail.com>
2023-01-12 20:41:30 +0900

    배열과오브젝트

--- main.js
+++ main.js
@@ -1,22 +1,71 @@
-console.log("hello javascript!!")
-console.log(console)
+// array
+const animals = ["lion","rabbit","monkey","dog","cat"];
+//const fruits = "apple, banana, strawberry"
+//animals[3]='tigar'
+//animals.push("tigar")
+//animals.unshift("dog") // 맨 앞에 위치
+//animals.pop();
+//console.log(Array.isArray(fruits));
 
+//console.log(animals.indexOf("monkey"))
 
-// var,let,const
+//splice, slice
+console.log(animals.splice(1,3));  // 1번째 인덱스 부터 3개
+//animals.splice(animals.indexOf("monkey"),3)
+//const sliceAnimals = animals.slice(0,2)
 
-// var는 글로벌한 이유로 사용을 지양할 것
-var name = "scalper"
+//object
+const monkey = {
+    name:'kiki',
+    age:8,
+    weight : 40,
+    gender : 'male',
+    isCute : true,
+    food:["banana","carrot","potato"],
+    home : {
+        location : "zoo",
+        size : "big",
+        price: 3000
+    }
+}
 
-//let
-// 변수의 값을 재정의 할 필요가 있을 때
-let score = 0;
-score =2;
+monkey.gender = "female"
 
-// const
-// 변하지 않은 값을 지정할 때, 블록 밖에서 사용x
-const name = "scalper"
-name = "codeScalper";
-console.log(name)  // 에러발생
+console.log(monkey['name'])
+console.log(monkey)
 
-// 블록 안에서 선언 된 것은 블록 안에서만 
-//사용 가능 
+//json
+console.log(monkey)
+const monkeyJSON = JSON.stringify(monkey);
+const parseJASON = JSON.parse(monkeyJSON)
+console.log(parseJASON.name)
+
+//미니 프로젝트
+//1. 과일 종류 배열
+const friuts = ["apple","banana","strawberry","graph"]
+//console.log(friuts)
+//console.log(Array.isArray(friuts))
+//console.log(friuts.splice(0,2))
+fruits1=friuts.slice(0,2)
+console.log(fruits1)
+
+//2. 직원 인적사항
+const person = {
+    age:29,
+    name:"paul",
+    number:"010-4555-1111",
+    hobby:["book","shopping","movie"],
+    family:{
+        father:"kim",
+        mother:"lian",
+        brother:"tom"
+    }
+} 
+
+console.log(person.age)
+
+//json
+
+const personJSON = JSON.stringify(person);
+const parseJASON1 = JSON.parse(personJSON);
+console.log(parseJASON1.name)
