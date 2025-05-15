075e5338a95decf93c96f86e23d9a820b41ca10d
eunjaelim <ieunjae361@gmail.com>
2025-05-15 19:47:56 +0900

    how to use switch statements in javascript

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -4,7 +4,7 @@ let numberOfDrumButtons = document.querySelectorAll(".drum").length;
 for(let i = 0; i< numberOfDrumButtons; i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-    alert("I got clicked!")
+    this.style.color = "white";
 });
 
 }
